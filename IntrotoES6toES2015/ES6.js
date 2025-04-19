61bdf3d1e5ad0c9e66692a86ab0f91cc0291f6e7
005LuxmiSaini <luxmisaini005@gmail.com>
2025-04-19 21:01:11 +0530

    modified

--- IntrotoES6toES2015/ES6.js
+++ IntrotoES6toES2015/ES6.js
@@ -49,6 +49,11 @@ for (var i = 0; i < 5; i++){
 console.log(i);
 */
 /*
+
+
+
+
+
 /// Lecture: Blocks and IIFES
 // ES6
 {
@@ -66,6 +71,11 @@ console.log(i);
 })();
 //console.log(c);*/
 /*
+
+
+
+
+
 // Lecture: Strings
 let firstName = 'John';
 let lastName = 'Smith';
@@ -86,6 +96,10 @@ console.log(n.startsWith('J'));
 console.log(n.endsWith('S'));
 console.log(n.includes('oh'));
 console.log(`${firstName}`.repeat(5));*/
+/*
+
+
+
 
 // Lecture: Arrow functions
 
@@ -111,4 +125,134 @@ ages6 = years.map((el, index) => {
     const age = now - el;
     return `Age element ${index + 1}: ${age}.`
 });
-console.log(ages6);
+console.log(ages6);*/
+
+
+/*
+// Lecture: Arrow  function 2
+
+//ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+       
+       var self = this; 
+        document.querySelector('#one').addEventListener('click', function(){
+            var str = 'This is box number '+ self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+};
+//box5.clickMe();
+
+
+
+//ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+       
+        
+        document.querySelector('#one').addEventListener('click', () => {
+            var str = 'This is box number '+ this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+};
+box6.clickMe();
+
+
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+       
+        
+        document.querySelector('#one').addEventListener('click', () => {
+            var str = 'This is box number '+ this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+};
+box66.clickMe();
+
+function Person(name){
+    this.name = name;
+}
+//ES5
+Person.prototype.myFrirnds5 = 
+function(friends){
+    var arr = friends.map(function(el)
+{
+    return this.name + ' is friends with ' + el;
+}.bind(this));
+  console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new
+Person('John').myFrirnds5(friends);
+
+
+function Person(name){
+    this.name = name;
+}
+
+
+//ES6
+
+Person.prototype.myFrirnds6= 
+function(friends){
+    var arr = friends.map(el =>
+   ` ${this.name} is friends with ${el}`);
+  console.log(arr);
+}
+
+
+ 
+new   Person('Mike').myFrirnds6(friends);    */ 
+
+/*
+//Lecture: Destructuring
+
+//ES5
+var john = ['John', 26];
+var name=john[0];
+var age = john[1];
+
+//ES6
+const [name, age] = ['John' , 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const{firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+
+function calcAgeRetirement(year){
+    const age = new
+    Date().getFullYear()- year;
+    return[age,65- age];
+}
+
+const [age2,retirement] = calcAgeRetirement(1990);
+console.log(age);
+console.log(retirement);
+
+
+*/
+
+// Lecture: Arrays
+
